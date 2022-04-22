1bb2f44d329f7b4d9a74da573e1c8f6751591b07
James Chao <jameschao@gmail.com>
2022-04-22 00:48:52 -0700

    Write out video infos to video infos file

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const credentials = require('./test/config');
 const { login, fetchUserVideoInfos } = require('./msstreams-utils');
-
+const { ensureDirExists, fileExists, readFromFile, writeToFile } = require('./file-utils');
 
 const main = async () => {
+    // basic credentials
     const account = process.env.TEST_ACCOUNT || credentials.account;
     const pwd = process.env.TEST_PWD || credentials.pwd;
     const userUuid = process.env.TEST_USER_UUID || credentials.userUuid;
 
+    // get the account token
     const token = await login({ account, pwd});
-    const videoInfos = await fetchUserVideoInfos(userUuid, token);
+
+    // paths
+    const userDir = `./output/${userUuid}`;
+    const videoInfosPath = `${userDir}/videos.json`;
+
+    // create user dir if needed
+    await ensureDirExists(userDir);
+
+    // fetch video infos if needed
+    let videoInfos;
+    if (fileExists(videoInfosPath)) {
+        videoInfos = readFromFile(videoInfosPath);
+    } else {
+        // get the user's video info
+        videoInfos = await fetchUserVideoInfos(userUuid, token);
+        writeToFile(videoInfosPath, videoInfos);
+    }
 
     console.log("VideoInfos", videoInfos);
 }
